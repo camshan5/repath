fe16b439afaca489826fd59e0cf1f6ba52aef680
Cameron Shannon <cameroncshannon@gmail.com>
2019-05-15 20:05:55 -0700

    Added functions that list

--- repath.py
+++ repath.py
@@ -3,28 +3,56 @@ from pathlib import Path, PurePath
 from bullet import Bullet, VerticalPrompt, ScrollBar
 
 
+def list_suffix():
+    # list file suffixes in path -> '.py'
+    path_list = []
+
+    cwd = Path.cwd()
+
+    for p in cwd.iterdir():
+        sfx = PurePath(p).suffix
+
+        # if suffix not an empty string, append to list
+        if sfx != "":
+            path_list.append(sfx)
+
+    return path_list
+
+
+def find_ext():
+
+    sfx_list = list(dict.fromkeys(list_suffix()))
+
+    cli = Bullet(prompt="\nSelect a file extension: ", choices=sfx_list, bullet="→")
+
+    result = cli.launch()
+
+    print(f"\nYou selected: {result}\n")
+
+    return result
+
+
 def find_dir():
     # py_path = Path.home() / "Developer" / "Language" / "Python"
 
+    file_ext = find_ext()
     current_dir = Path.cwd()
+
     if current_dir.is_dir():
 
         # list of all files in directory
         path_list = []
-        for p in current_dir.iterdir():
 
+        for p in current_dir.iterdir():
             # string representation of path names
             p = PurePath(p).name
 
-            # append only .py files to list
-            if PurePath(p).suffix == ".py":
+            # append selected files from find_ext() to list
+            if PurePath(p).suffix == file_ext:
                 path_list.append(p)
 
-        print(path_list)
-
         cli = VerticalPrompt(
-            [Bullet("Please select a file or path", choices=path_list, bullet="→")],
-            spacing=1,
+            [Bullet("Select a Path", choices=path_list, bullet="→")], spacing=1
         )
 
         result = cli.launch()
@@ -35,8 +63,5 @@ def find_dir():
         print("Directory does not exist")
 
 
-
-
 if __name__ == "__main__":
-    # tree(Path.cwd())
     find_dir()
