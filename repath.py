ee9099770518e4fa7c36d67260779729c8367c2d
Cameron Shannon <cameroncshannon@gmail.com>
2019-05-15 21:37:38 -0700

    added clear function, cleaned up remaining functions

--- repath.py
+++ repath.py
@@ -1,15 +1,32 @@
 # Analyze Files and Paths
+from os import system, name
+
 from pathlib import Path, PurePath
 from bullet import Bullet, VerticalPrompt, ScrollBar
 
 
-def list_suffix():
-    # list file suffixes in path -> '.py'
+cwd = Path.cwd()
+
+
+def clear():
+    # for windows
+    if name == "nt":
+        _ = system("cls")
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system("clear")
+
+
+def build_path():
+    # py_path = Path.home() / "Developer" / "Language" / "Python"
+    pass
+
+
+def list_sfx():
     path_list = []
 
-    cwd = Path.cwd()
-
     for p in cwd.iterdir():
+        # The file extension of the final component, if any -> .py
         sfx = PurePath(p).suffix
 
         # if suffix not an empty string, append to list
@@ -20,26 +37,21 @@ def list_suffix():
 
 
 def find_ext():
-
-    sfx_list = list(dict.fromkeys(list_suffix()))
-
+    sfx_list = list(dict.fromkeys(list_sfx()))
     cli = Bullet(prompt="\nSelect a file extension: ", choices=sfx_list, bullet="→")
-
     result = cli.launch()
 
-    print(f"\nYou selected: {result}\n")
+    print(f"\nYou selected the '{result}' file suffix\n ")
 
     return result
 
 
 def find_dir():
-    # py_path = Path.home() / "Developer" / "Language" / "Python"
-
+    clear()
     file_ext = find_ext()
     current_dir = Path.cwd()
 
     if current_dir.is_dir():
-
         # list of all files in directory
         path_list = []
 
@@ -56,7 +68,6 @@ def find_dir():
         )
 
         result = cli.launch()
-
         print(f"You selected: {result}")
 
     else:
